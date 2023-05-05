795b449051350532c37e74a8e851bbad030a5edd
Ali Abdullah <ali.abdullah.skipq@gmail.com>
2023-05-06 00:36:32 +0500

    add Story schema

---
+++ digital-stories-backend/models/Story.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const StorySchema = mongoose.Schema({
+    fullname:{
+        type: String,
+        required:true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    dateofBirth:{
+        type: Date,
+        required: true
+    },
+    gender:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Story", StorySchema);
